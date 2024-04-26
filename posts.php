0f6a2c6afaee5e34de42979981c2fb681c6587ef
Amine Mokeddem <mokeddemamine1707@gmail.com>
2024-04-26 17:18:45 +0200

    create and start the posts page

---
+++ posts.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Posts</title>
+</head>
+<body>
+    <div class="container">
+        <div class="posts">
+            <div class="row">
+                <div class="col col-md-6 col-lg-3">
+                    laksjd fkojklasjdklfklajsdf
+                </div>
+                <div class="col col-md-6 col-lg-3">
+                    laksjd fkojklasjdklfklajsdf
+                </div>
+                <div class="col col-md-6 col-lg-3">
+                    laksjd fkojklasjdklfklajsdf
+                </div>
+                <div class="col col-md-6 col-lg-3">
+                    laksjd fkojklasjdklfklajsdf
+                </div>
+                <div class="col col-md-6 col-lg-3">
+                    laksjd fkojklasjdklfklajsdf
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
