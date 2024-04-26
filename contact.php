bb2d41cd02f7c7695a9abddb810b4b9105be3bdb
Amine Mokeddem <mokeddemamine1707@gmail.com>
2024-04-26 17:13:09 +0200

    modify the about and contact pages

---
+++ contact.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Us</title>
+</head>
+<body>
+    <section class="contact-us">
+        <div class="container">
+            <div class="text-center">
+                <form action="">
+                    <input type="text" name="full-name" id="" class="form-control">
+                </form>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
