a02b6afd767f592154eb5b45c6ce174d35cd5930
Amine Mokeddem <mokeddemamine1707@gmail.com>
2024-04-26 15:52:53 +0200

    add the main index.php file

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test Git and Github</title>
+</head>
+<body>
+    <div class="text-center">assi l azaoui newfel</div>
+</body>
+</html>
